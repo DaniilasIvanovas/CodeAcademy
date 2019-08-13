6bf1403cb7721c5cd1016198be74d98329a0b2ec
Daniilas <47268986+DaniilasIvanovas@users.noreply.github.com>
2019-08-13 11:28:04 +0300

    Changes
    Added user system detection so script can tun on Mac. 
    Added file moving to existing dirs with file type names.

---
+++ modules/istorija.py
@@ -0,0 +1,15 @@
+import datetime
+
+
+def create_file():
+    try:
+        with open('Edit history.txt', 'w') as failas:
+            failas.write('All moved files will be shown here. \n')
+    except FileExistsError:
+        return False
+
+
+def mark_file(filename, prev_path, current_path):
+    with open('Edit history.txt', 'a') as failas:
+        failas.write('<------> \n')
+        failas.write('On ' + str(datetime.datetime.now()) + ' file ' + filename + ' was moved from ' + prev_path + ' to ' + current_path + '\n')
--- modules/langas.py
+++ modules/langas.py
@@ -2,10 +2,19 @@ from tkinter import *
 import os
 import shutil
 from modules import istorija
+from sys import platform
+
+
+def check(path):
+    if os.path.isdir(path):
+        return
+    else:
+        os.mkdir(path)
 
 
 class Langas:
     def __init__(self):
+        self.os = ''
         self.root = Tk()
         self.root.title('Rūšiuoklė')
         self.root.geometry('700x150')
@@ -35,86 +44,153 @@ class Langas:
         self.uzrasas_error.config(font=('Courrier', 11))
         self.uzrasas_error.grid(row=4, columnspan=2)
 
-        mygtukas = Button(self.root, text='Rusiuoti', command=lambda: self.search_type())
+        mygtukas = Button(self.root, text='Rusiuoti', command=lambda: self.check_platform())
         mygtukas.grid(row=3, column=3, sticky=W)
 
-    def search_type(self):
+    def check_platform(self):
+        if platform == 'linux' or platform == 'linux2':  # Linux
+            self.search_type()
+            self.os = 'linux'
+        elif platform == 'darwin':  # OS X
+            self.search_type_mac()
+            self.os = 'mac'
+        elif platform == 'win32':  # Windows
+            self.search_type()
+            self.os = 'win'
+
+    def search_type(self):  # Check for file types, and create dirs for them on WINDOWS OR LINUX
         self.uzrasas_error['text'] = ''
         istorija.create_file()
         global a
         a = os.listdir(self.path_laukas.get())
-        try:
-            if any('.txt' in s for s in a):
-                global txt_path, txt_list
-                txt_list = []
-                txt_path = self.path_laukas.get() + '\\Txt'
-                os.mkdir(self.path_laukas.get() + '\\Txt')
-            if any('.jpg' in s for s in a):
-                os.mkdir(self.path_laukas.get() + '\\Jpg')
-                global jpg_path
-                jpg_path = self.path_laukas.get() + '\\Jpg'
-                global jpg_list
-                jpg_list = []
-            if any('.png' in s for s in a):
-                os.mkdir(self.path_laukas.get() + '\\Png')
-                global png_path
-                png_path = self.path_laukas.get() + '\\Png'
-                global png_list
-                png_list = []
-            if any('.docx' in s for s in a):
-                os.mkdir(self.path_laukas.get() + '\\Word')
-                global docx_path
-                docx_path = self.path_laukas.get() + '\\Word'
-                global docx_list
-                docx_list = []
-            if any('.pptx' in s for s in a):
-                os.mkdir(self.path_laukas.get() + '\\Power Point')
-                global pptx_path
-                pptx_path = self.path_laukas.get() + '\\Power Point'
-                global pptx_list
-                pptx_list = []
-            if any('.xlsx' in s for s in a):
-                os.mkdir(self.path_laukas.get() + '\\Excel')
-                global xlsx_path
-                xlsx_path = self.path_laukas.get() + '\\Excel'
-                global xlsx_list
-                xlsx_list = []
-            if any('.pdf' in s for s in a):
-                os.mkdir(self.path_laukas.get() + '\\Pdf')
-                global pdf_path
-                pdf_path = self.path_laukas.get() + '\\Pdf'
-                global pdf_list
-                pdf_list = []
-            if any('.rar' in s for s in a):
-                os.mkdir(self.path_laukas.get() + '\\Rar Archive')
-                global rar_path
-                rar_path = self.path_laukas.get() + '\\Rar Archive'
-                global rar_list
-                rar_list = []
-            if any('.mp4' in s for s in a):
-                os.mkdir(self.path_laukas.get() + '\\Mp4')
-                global mp4_path
-                mp4_path = self.path_laukas.get() + '\\Mp4'
-                global mp4_list
-                mp4_list = []
-            if any('.mov' in s for s in a):
-                os.mkdir(self.path_laukas.get() + '\\Mov')
-                global mov_path
-                mov_path = self.path_laukas.get() + '\\Mov'
-                global mov_list
-                mov_list = []
-            if any('.zip' in s for s in a):
-                os.mkdir(self.path_laukas.get() + '\\Zip')
-                global zip_path
-                zip_path = self.path_laukas.get() + '\\Zip'
-                global zip_list
-                zip_list = []
-            self.sort_files()
-        except FileExistsError:
-                self.uzrasas_error['text'] = 'Please delete all directories named by file endings'
+        if any('.txt' in s for s in a):  # Txt
+            global txt_path, txt_list
+            txt_list = []
+            txt_path = self.path_laukas.get() + '\\Txt'
+            check(txt_path)
+        if any('.jpg' in s for s in a):  # Jpg
+            global jpg_path, jpg_list
+            jpg_list = []
+            jpg_path = self.path_laukas.get() + '\\Jpg'
+            check(jpg_path)
+        if any('.png' in s for s in a):  # Png
+            global png_path, png_list
+            png_path = self.path_laukas.get() + '\\Png'
+            png_list = []
+            check(png_path)
+        if any('.docx' in s for s in a):  # Word
+            global docx_path, docx_list
+            docx_path = self.path_laukas.get() + '\\Word'
+            docx_list = []
+            check(docx_path)
+        if any('.pptx' in s for s in a):  # Power Point
+            global pptx_path,pptx_list
+            pptx_path = self.path_laukas.get() + '\\Power Point'
+            pptx_list = []
+            check(pptx_path)
+        if any('.xlsx' in s for s in a):  # Excel
+            os.mkdir(self.path_laukas.get() + '\\Excel')
+            global xlsx_path, xlsx_list
+            xlsx_path = self.path_laukas.get() + '\\Excel'
+            xlsx_list = []
+            check(xlsx_path)
+        if any('.pdf' in s for s in a):  # Pdf
+            os.mkdir(self.path_laukas.get() + '\\Pdf')
+            global pdf_path, pdf_list
+            pdf_path = self.path_laukas.get() + '\\Pdf'
+            pdf_list = []
+            check(pdf_path)
+        if any('.rar' in s for s in a):  # Rar
+            os.mkdir(self.path_laukas.get() + '\\Rar Archive')
+            global rar_path, rar_list
+            rar_path = self.path_laukas.get() + '\\Rar Archive'
+            rar_list = []
+            check(rar_path)
+        if any('.mp4' in s for s in a):  # Mp4
+            os.mkdir(self.path_laukas.get() + '\\Mp4')
+            global mp4_path, mp4_list
+            mp4_path = self.path_laukas.get() + '\\Mp4'
+            mp4_list = []
+            check(mp4_path)
+        if any('.mov' in s for s in a):  # Mov
+            os.mkdir(self.path_laukas.get() + '\\Mov')
+            global mov_path, mov_list
+            mov_path = self.path_laukas.get() + '\\Mov'
+            mov_list = []
+            check(mov_path)
+        if any('.zip' in s for s in a):  # Zip
+            os.mkdir(self.path_laukas.get() + '\\Zip')
+            global zip_path, zip_list
+            zip_path = self.path_laukas.get() + '\\Zip'
+            zip_list = []
+            check(zip_path)
 
+        self.sort_files()
 
-    def sort_files(self):
+    def search_type_mac(self):  # Check for file types, and create dirs for them on MAC
+        self.uzrasas_error['text'] = ''
+        istorija.create_file()
+        global a
+        a = os.listdir(self.path_laukas.get())
+        if any('.txt' in s for s in a):  # Txt
+            global txt_path, txt_list
+            txt_list = []
+            txt_path = self.path_laukas.get() + '\Txt'
+            check(txt_path)
+        if any('.jpg' in s for s in a):  # Jpg
+            global jpg_path, jpg_list
+            jpg_list = []
+            jpg_path = self.path_laukas.get() + '\Jpg'
+            check(jpg_path)
+        if any('.png' in s for s in a):  # Png
+            global png_path, png_list
+            png_path = self.path_laukas.get() + '\Png'
+            png_list = []
+            check(png_path)
+        if any('.docx' in s for s in a):  # Word
+            global docx_path, docx_list
+            docx_path = self.path_laukas.get() + '\Word'
+            docx_list = []
+            check(docx_path)
+        if any('.pptx' in s for s in a):  # Power Point
+            global pptx_path,pptx_list
+            pptx_path = self.path_laukas.get() + '\Power Point'
+            pptx_list = []
+            check(pptx_path)
+        if any('.xlsx' in s for s in a):  # Excel
+            global xlsx_path, xlsx_list
+            xlsx_path = self.path_laukas.get() + '\Excel'
+            xlsx_list = []
+            check(xlsx_path)
+        if any('.pdf' in s for s in a):  # Pdf
+            global pdf_path, pdf_list
+            pdf_path = self.path_laukas.get() + '\Pdf'
+            pdf_list = []
+            check(pdf_path)
+        if any('.rar' in s for s in a):  # Rar
+            global rar_path, rar_list
+            rar_path = self.path_laukas.get() + '\Rar Archive'
+            rar_list = []
+            check(rar_path)
+        if any('.mp4' in s for s in a):  # Mp4
+            global mp4_path, mp4_list
+            mp4_path = self.path_laukas.get() + '\Mp4'
+            mp4_list = []
+            check(mp4_path)
+        if any('.mov' in s for s in a):  # Mov
+            global mov_path, mov_list
+            mov_path = self.path_laukas.get() + '\Mov'
+            mov_list = []
+            check(mov_path)
+        if any('.zip' in s for s in a):  # Zip
+            global zip_path, zip_list
+            zip_path = self.path_laukas.get() + '\Zip'
+            zip_list = []
+            check(zip_path)
+
+        self.sort_files()
+
+    def sort_files(self):  # Sort all found files into lists
         for each in a:
             if '.txt' in each:
                 txt_list.append(each)
@@ -138,9 +214,12 @@ class Langas:
                 mov_list.append(each)
             if '.zip' in each:
                 zip_list.append(each)
-        self.move_files()
+        if self.os == 'linux' or self.os == 'win':
+            self.move_files()
+        else:
+            self.move_files_mac()
 
-    def move_files(self):
+    def move_files(self):  # Move files from current dir to 'File type' dirs WIN OR LINUX
         if 'txt_list' in globals() or 'txt_list' in locals():
             for each in txt_list:
                 istorija.mark_file(each, self.path_laukas.get(), txt_path)
@@ -185,6 +264,53 @@ class Langas:
             for each in zip_list:
                 istorija.mark_file(each, self.path_laukas.get(), zip_path)
                 shutil.move(self.path_laukas.get() + '\\' + each, zip_path)
+        self.done()
+
+    def move_files_mac(self):  # Move files from current dir to 'File type' dirs MAC
+        if 'txt_list' in globals() or 'txt_list' in locals():
+            for each in txt_list:
+                istorija.mark_file(each, self.path_laukas.get(), txt_path)
+                shutil.move(self.path_laukas.get() + each, txt_path)
+        if 'jpg_list' in globals() or 'jpg_list' in locals():
+            for each in jpg_list:
+                istorija.mark_file(each, self.path_laukas.get(), jpg_path)
+                shutil.move(self.path_laukas.get() + each, jpg_path)
+        if 'png_list' in globals() or 'png_list' in locals():
+            for each in png_list:
+                istorija.mark_file(each, self.path_laukas.get(), png_path)
+                shutil.move(self.path_laukas.get() + each, png_path)
+        if 'docx_list' in globals() or 'docx_list' in locals():
+            for each in docx_list:
+                istorija.mark_file(each, self.path_laukas.get(), docx_path)
+                shutil.move(self.path_laukas.get() + each, docx_path)
+        if 'pptx_list' in globals() or 'pptx_list' in locals():
+            for each in pptx_list:
+                istorija.mark_file(each, self.path_laukas.get(), pptx_path)
+                shutil.move(self.path_laukas.get() + each, pptx_path)
+        if 'xlsx_list' in globals() or 'xlsx_list' in locals():
+            for each in xlsx_list:
+                istorija.mark_file(each, self.path_laukas.get(), xlsx_path)
+                shutil.move(self.path_laukas.get() + each, xlsx_path)
+        if 'pdf_list' in globals() or 'pdf_list' in locals():
+            for each in pdf_list:
+                istorija.mark_file(each, self.path_laukas.get(), pdf_path)
+                shutil.move(self.path_laukas.get() + each, pdf_path)
+        if 'rar_list' in globals() or 'rar_list' in locals():
+            for each in rar_list:
+                istorija.mark_file(each, self.path_laukas.get(), rar_path)
+                shutil.move(self.path_laukas.get() + each, rar_path)
+        if 'mp4_list' in globals() or 'mp4_list' in locals():
+            for each in mp4_list:
+                istorija.mark_file(each, self.path_laukas.get(), mp4_path)
+                shutil.move(self.path_laukas.get() + each, mp4_path)
+        if 'mov_list' in globals() or 'mov_list' in locals():
+            for each in mov_list:
+                istorija.mark_file(each, self.path_laukas.get(), mov_path)
+                shutil.move(self.path_laukas.get() + each, mov_path)
+        if 'zip_list' in globals() or 'zip_list' in locals():
+            for each in zip_list:
+                istorija.mark_file(each, self.path_laukas.get(), zip_path)
+                shutil.move(self.path_laukas.get() + each, zip_path)
         self.done()
 
     def done(self):
